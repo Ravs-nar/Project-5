e345eddd23a0efffaf2fbcdd193f6ec741645ddd
Ravi Narayan <ravi.narayan09@gmail.com>
2021-11-29 15:23:12 -0800

    done

--- js/apps.js
+++ js/apps.js
@@ -1,25 +1,3 @@
 baguetteBox.run('.gallery');
 
-const search = new Filter('search','A picture of a field');
-
-const search = new Filter('search','A picture of a lake');
-
-const search = new Filter('search','A picture of a field');
-
-const search = new Filter('search','A picture of a frozen lake');
-
-const search = new Filter('search','A picture of a gorge');
-
-const search = new Filter('search','A picture of a forest');
-
-const search = new Filter('search','A picture of a green forest');
-
-const search = new Filter('search','A picture of a desert');
-
-const search = new Filter('search','A picture of a pathway');
-
-const search = new Filter('search','A picture of a sunset');
-
-const search = new Filter('search','A picture of a view');
-
-const search = new Filter('search','A picture of a mountain');
+const search = new Filter('search', 'data-caption');
